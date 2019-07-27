bedc5e02aeb6139767e05550563254c10c373262
Spencer Riner <spencerriner@gmail.com>
2019-07-26 20:06:28 -0500

    Fix syntax in config

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'qwertyuiopasdfghjkl'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-            'mysql://teashop:1qa!QA2ws@WS3ed#ED4rf$RF@localhost/teashop')
+            'mysql://teashop:1qa!QA2ws@WS3ed#ED4rf$RF@localhost/teashop'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
