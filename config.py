bd0b98d6f10f3997194f9fa23a5a5b16a8351347
Spencer Riner <spencerriner@gmail.com>
2019-07-26 19:58:49 -0500

    Don't track db modifications

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'qwertyuiopasdfghjkl'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
             'mysql://teashop:1qa!QA2ws@WS3ed#ED4rf$RF@localhost/teashop')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
