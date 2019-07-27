4ae23a64e324041241b82c7190cf83badc096072
Spencer Riner <spencerriner@gmail.com>
2019-07-26 20:05:45 -0500

    More db config

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from app import db
+
+class Employee(db.Model):
+    employee_id = db.Column(db.Integer, primary_key=True)
+    employee_name = db.Column(db.String(128), index=True, unique=True)
+    phone = db.Column(db.String(64), index=True, unique=True)
+    password = db.Column(db.String(64))
+
+    def __repr__(self):
+        return '<Employee {}'>.format(self.employee_id)
