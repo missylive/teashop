375b894d188a2ece9476b893fc3df1ce1d327b55
Spencer Riner <spencerriner@gmail.com>
2019-07-26 20:27:33 -0500

    Add pw hashing

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Employee(db.Model):
     employee_id = db.Column(db.Integer, primary_key=True)
@@ -8,3 +9,9 @@ class Employee(db.Model):
 
     def __repr__(self):
         return '<Employee {}>'.format(self.employee_id)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
