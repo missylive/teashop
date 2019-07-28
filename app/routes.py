12e165d2d3d0030581b5ca9cb4c73050e2b2dd85
Spencer Riner <spencerriner@gmail.com>
2019-07-27 22:11:48 -0500

    Menu item mgmt

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, flash, redirect
 from app import app
 from app import db
 from app.forms import LoginForm
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import Employee, MenuItem, AddOnItem
 
 @app.route('/')
@@ -22,6 +22,13 @@ def mgmt():
     item_list = MenuItem.query.all()
     addon_list = AddOnItem.query.all()
     return render_template('mgmt.html', title='Management', item_list=item_list, addon_list=addon_list)
+@app.route('/<menu_item_number>')
+@login_required
+def menuitem(menu_item_number):
+    item_list = MenuItem.query.all()
+    menu_item = MenuItem.query.filter_by(menu_item_number=menu_item_number).first_or_404()
+    return render_template('mgmtmenu.html', menu_item=menu_item, item_list=item_list)
+@app.route('/addon/<add_on_number>')
 
 
 @app.route('/login', methods=['GET', 'POST'])
